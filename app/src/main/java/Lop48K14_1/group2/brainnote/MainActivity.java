5648e156184aec62a700f04fb0000a23f56b1ee0
didudua <126039750+didudua@users.noreply.github.com>
2025-03-18 00:11:19 +0700

    welcome

--- app/src/main/java/Lop48K14_1/group2/brainnote/MainActivity.java
+++ app/src/main/java/Lop48K14_1/group2/brainnote/MainActivity.java
@@ -1,4 +1,14 @@
 package Lop48K14_1.group2.brainnote;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
 }
