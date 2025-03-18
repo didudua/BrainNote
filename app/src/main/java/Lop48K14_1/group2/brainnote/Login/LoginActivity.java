a28bbe0885d083547b5e729342cc6be7c1db9a00
didudua <126039750+didudua@users.noreply.github.com>
2025-03-18 16:43:20 +0700

    welcome

---
+++ app/src/main/java/Lop48K14_1/group2/brainnote/Login/LoginActivity.java
@@ -0,0 +1,45 @@
+package Lop48K14_1.group2.brainnote.Login;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import Lop48K14_1.group2.brainnote.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+        // Set up button click listeners
+        CardView googleSignInButton = findViewById(R.id.googleSignInButton);
+        CardView createAccountButton = findViewById(R.id.createAccountButton);
+        CardView signInButton = findViewById(R.id.signInButton);
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Google Sign In Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Create Account Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Sign In Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
